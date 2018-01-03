af7cf303a8c617bf4be87305e0fef565f3fffdd4
Hzg <753166446@qq.com>
2018-01-03 22:43:15 +0800

    init

--- src/app.js
+++ src/app.js
@@ -1,52 +1,41 @@
 import {createStore} from 'redux';
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-const counter = (state=0, action) => {
-    switch(action.type){
-        case 'increase':
-            return state+1;
-        case 'decrease':
-            return state-1;
+
+let gid = 0;
+const todos = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return [...state, {
+                id: ++gid,
+                text: action.text,
+                completed: false,
+            }]
+        case 'TOGGLE_TOOD': 
+           return state.map((todo) => {//map返回一个
+                if(todo.id == action.id){
+                    return Object.assign({}, todo, {
+                        completed: !todo.completed
+                    })
+                }
+                return todo;
+            })
         default :
             return state;
     }
 }
 
+let store = createStore(todos);
+console.log("current", store.getState());
+console.log('dispach ADD_TODO');
+store.dispatch({
+    type: 'ADD_TODO',
+    text: 'mango'
+})
+console.log('current:', store.getState());
+console.log('dispach TOGGLE_TOOD');
 
-
-
-const store = createStore(counter);
-
-class Counter extends Component {
-    render() {
-        return (
-            <div>
-                <h1>{store.getState()}</h1>
-                <button onClick={()=>{
-                    store.dispatch({
-                        type: 'decrease'
-                    })
-                }}>-</button>
-                <button onClick={()=>{
-                    store.dispatch({
-                        type: 'increase'
-                    })
-                }}>+</button>                
-            </div>
-        )
-    }
-}
-
-const render = () => {
-    ReactDom.render(
-        <Counter  />,
-        document.getElementById('root')
-    )
-    // document.getElementsByTagName('body')[0].innerHTML = '<h1>'+store.getState()+'</h1>';
-}
-
-render();
-
-const unsubscribe = store.subscribe(render);//监听事件，一旦state发生改变，就会自动执行函数
-
-
+store.dispatch({
+    type: 'TOGGLE_TOOD',
+    id: 1
+})
